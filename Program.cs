00bd3459346fa614f45744e27bee440513d72ec3
James <151320422+chrysoprasus@users.noreply.github.com>
2025-08-05 12:14:17 -0400

    Added Files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter name: ");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine(name);
+        }
+    }
+}
